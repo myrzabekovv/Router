ab2f8ad26ba2068aee1b87c17c1c9cc12d86970d
myrzabekovv <akim.myrzabekov06@mail.ru>
2023-03-16 21:15:02 +0600

    add item

--- lesson 6/src/pages/blog/item.jsx
+++ lesson 6/src/pages/blog/item.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+
 export const BlogItemPage = () => {
+  const {id} = useParams()
+  const [post, setPost] = useState(null)
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(res => res.json())
+      .then((data) => {
+        setPost(data)
+      })
+  }, [id])
   return (
     <div>
-      BlogItemPage
+      <h1>{post?.title}</h1>
+      <p>{post?.body}</p>
     </div>
   )
 }
